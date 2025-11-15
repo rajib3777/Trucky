27e4e22f6ca0a38e0f96f227890e48ebafea3cef
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-11-16 01:56:02 +0600

    Update urls.py

--- trips/urls.py
+++ trips/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import TripPlanView
+from django.http import JsonResponse
+
+def ping(request):
+    return JsonResponse({"status": "ok", "message": "Alive"})
 
 urlpatterns = [
     path("plan/", TripPlanView.as_view(), name="trip-plan"),
+    path("ping/", ping, name="ping"),
 ]
