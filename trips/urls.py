f7be2010b6b33d58edc7c30ae324a3186a546512
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-11-16 02:10:17 +0600

    Add TripPlanView import and ping function

--- trips/urls.py
+++ trips/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from django.http import JsonResponse
+from .views import TripPlanView
+from .views import ping
 
 def ping(request):
     return JsonResponse({"status": "ok", "message": "Alive"})
