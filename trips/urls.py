0fd220b71d8ebab0d00e37f4768930d1327ef269
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-11-16 02:15:21 +0600

    Refactor imports in urls.py

--- trips/urls.py
+++ trips/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
-from django.http import JsonResponse
-from .views import TripPlanView
-from .views import ping
-
-def ping(request):
-    return JsonResponse({"status": "ok", "message": "Alive"})
+from .views import TripPlanView, ping
 
 urlpatterns = [
     path("plan/", TripPlanView.as_view(), name="trip-plan"),
     path("ping/", ping, name="ping"),
 ]
+
