8c4b0290974b6441809572960b8d727924a91f38
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-11-15 22:42:47 +0600

    req update

--- trucky/settings.py
+++ trucky/settings.py
@@ -70,11 +70,19 @@ ASGI_APPLICATION = "trucky.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='',
+        conn_max_age=600
+    )
 }
 
 
